abf837643a9c447482cd605c5176bfdf564e26b9
mascaradee <lamascaradee@gmail.com>
2023-05-31 15:16:57 +0900

    컴포넌트 없이 통으로 구현

--- vuejs/src/Todo.vue
+++ vuejs/src/Todo.vue
@@ -3,17 +3,18 @@ import { ref } from 'vue'
 import TodoItem from './components/TodoItem.vue';
 
 // 조회
-const todos = ref(getStore())
+const todos = ref(getStore() || [])
+
 
 // 등록
 const content = ref(null)
 
 function registTodo(event) {
-    let todo = { checked: false, content: content.value, seq: getSeq() }
     if (event.key === 'Enter' && content.value != null) {
-        let todo = { checked: false, content: content.value, seq: getSeq() }
+        const todo = { checked: false, content: content.value, seq: getSeq() }
         todos.value = [...todos.value, todo]
         setStore(todos.value);
+        clear()
     }
 }
 
@@ -21,6 +22,29 @@ function getSeq() {
     let d = new Date();
     return `${d.getFullYear()}${d.getMonth() + 1}${d.getDay()}${d.getHours()}${d.getMinutes()}${d.getSeconds()}${d.getMilliseconds()}`
 }
+
+function clear() {
+    content.value = ''
+}
+
+// 체크 수정
+function updateCheck(todo) {
+    todo.checked = !checkedTodo.checked
+    setStore(todos.value)
+}
+// 내용수정
+function updateContent(event, todo) {
+    if (event.key === 'Enter') {
+        todo.content = event.target.value
+        setStore(todos.value)
+    }
+}
+// 삭제
+function deleteTodo(seq) {
+    todos.value = todos.value.filter(e => e.seq !== seq)
+    setStore(todos.value)
+}
+
 
 // 로컬 스토리지
 function getStore() {
@@ -42,14 +66,14 @@ function setStore(todos) {
             <div class="todo-input-box">
                 <input type="text" class="todo-input" v-model="content" @keydown="registTodo" placeholder="할 일을 입력 후 엔터">
             </div>
-            <ul class="todo-list" id="todo-list">
-                <li v-for="todo in todos" :key="todo.seq" class="todo-item" id="todo-item">
-                    <input :value="todo.seq" type="hidden" id="seq">
-                    <div class="checkbox" id="checked">{{ todo.checked ? "✔" : "" }}</div>
-                    <div id="content">
-                        <input :value="todo.content" id="content-update" class="todo">
+            <ul class="todo-list">
+                <li v-for="todo in todos" :key="todo.seq" class="todo-item">
+                    <input :value="todo.seq" type="hidden">
+                    <div class="checkbox" @click="updateCheck(todo)">{{ todo.checked ? "✔" : "" }}</div>
+                    <div>
+                        <input :value="todo.content" @keydown="updateContent($event, todo)" class="todo">
                     </div>
-                    <button class="delBtn" id="deleted">x</button>
+                    <button @click="deleteTodo(todo.seq)" class="delBtn">x</button>
                 </li>
             </ul>
         </div>
