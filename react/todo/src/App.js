b71d9fa165d69abd73fa489b3358f8146b75786f
mascaradee <lamascaradee@gmail.com>
2023-06-08 12:08:52 +0900

    테스트

--- react/todo/src/App.js
+++ react/todo/src/App.js
@@ -1,8 +1,12 @@
 import TodoApp from './components/TodoApp'
 import './App.css'
+import Parent from './components/Parent';
 export default function App() { // 메인 컴포넌트
   return (
-    <TodoApp />
+    <>
+      <TodoApp />
+      <Parent />
+    </>
   );
 }
 
