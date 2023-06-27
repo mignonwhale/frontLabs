3b6d5809c25553b450056456c432eae36aa94843
mascaradee <lamascaradee@gmail.com>
2023-06-27 19:23:00 +0900

    todo 기본 템플릿 적용

--- vue-typescript/src/App.vue
+++ vue-typescript/src/App.vue
@@ -1,14 +1,23 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-  </header>
-
-  <main>
-  </main>
+  <div class="todo-wrapper">
+    <div class="todo-title">todo app</div>
+    <div class="todo-box">
+      <div class="todo-input-box">
+        <input type="text" class="todo-input" placeholder="할 일을 입력 후 엔터" />
+      </div>
+      <ul class="todo-list">
+        <li class="todo-item checked">
+          <div class="checkbox"></div>
+          <div class="content">
+            <input class="todo" />
+          </div>
+          <button class="delBtn">x</button>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
