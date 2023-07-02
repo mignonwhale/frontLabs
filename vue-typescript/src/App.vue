d16abe5cd20fa6bbbed3dee8fd17965b9add00a1
mascaradee <lamascaradee@gmail.com>
2023-07-02 21:07:08 +0900

    update

--- vue-typescript/src/App.vue
+++ vue-typescript/src/App.vue
@@ -1,4 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const VUE_TS_TODOS = 'vueTsTodos';
+const todos = ref(JSON.parse(localStorage.getItem(VUE_TS_TODOS) || '[]'));
+
+const props = defineProps({
+  id: String,
+  content: String,
+  checked: Boolean
+});
+
+
+</script>
 
 <template>
   <div class="todo-wrapper">
@@ -8,10 +21,10 @@
         <input type="text" class="todo-input" placeholder="할 일을 입력 후 엔터" />
       </div>
       <ul class="todo-list">
-        <li class="todo-item checked">
-          <div class="checkbox"></div>
+        <li v-for="todo in todos " :key="todo.id" class="todo-item" :class="todo.checked ? 'checked' : ''">
+          <div class="checkbox">{{ todo.checked ? '✔' : '' }}</div>
           <div class="content">
-            <input class="todo" />
+            <input type="text" class="todo" v-model="todo.content" />
           </div>
           <button class="delBtn">x</button>
         </li>
